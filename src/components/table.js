913e3c4817759746630e7f2a5c0972752516bb75
Alec Rewinkel <alecrewinkel@gmail.com>
2022-07-12 20:38:43 -0600

    mapping

--- src/components/table.js
+++ src/components/table.js
@@ -2,6 +2,12 @@ import { useState } from "react";
 import "../App.css";
 
 function Table({ info }) {
+  const data = [
+    { name: "Anom", age: 19, gender: "Male" },
+    { name: "Megha", age: 19, gender: "Female" },
+    { name: "Subham", age: 25, gender: "Male" },
+  ];
+  //   console.log(data);
   console.log(info);
   return (
     <div className="App">
@@ -15,33 +21,21 @@ function Table({ info }) {
           <th>Credit</th>
           <th>Description</th>
         </tr>
-        <tr>
-          <td>Anom</td>
-          <td>19</td>
-          <td>Male</td>
-          <td>Anom</td>
-          <td>19</td>
-          <td>Male</td>
-        </tr>
-        <tr>
-          <td>Megha</td>
-          <td>19</td>
-          <td>Female</td>
-          <td>Anom</td>
-          <td>19</td>
-          <td>Male</td>
-        </tr>
-        <tr>
-          <td>Subham</td>
-          <td>25</td>
-          <td>Male</td>
-          <td>Anom</td>
-          <td>19</td>
-          <td>Male</td>
-        </tr>
+        {data.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.name}</td>
+              <td>{val.age}</td>
+              <td>{val.gender}</td>
+            </tr>
+          );
+        })}
       </table>
+      {/* <h2>{info[0].catagory}</h2> */}
     </div>
   );
 }
 
 export default Table;
+
+//data is stored in info, need to figure out how to map through the data and pass it into the table tha you created, you are able to print the data, just need to get the map to work.
