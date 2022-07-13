dfa43e246d52fcfbf12fd8b1858787377e20594f
Alec Rewinkel <alecrewinkel@gmail.com>
2022-07-12 19:59:33 -0600

    change

--- src/components/table.js
+++ src/components/table.js
@@ -6,6 +6,40 @@ function Table({ info }) {
   return (
     <div className="App">
       <h2>Ella dog</h2>
+      <table>
+        <tr>
+          <th>Catagory</th>
+          <th>Date</th>
+          <th>Cost</th>
+          <th>Debit</th>
+          <th>Credit</th>
+          <th>Description</th>
+        </tr>
+        <tr>
+          <td>Anom</td>
+          <td>19</td>
+          <td>Male</td>
+          <td>Anom</td>
+          <td>19</td>
+          <td>Male</td>
+        </tr>
+        <tr>
+          <td>Megha</td>
+          <td>19</td>
+          <td>Female</td>
+          <td>Anom</td>
+          <td>19</td>
+          <td>Male</td>
+        </tr>
+        <tr>
+          <td>Subham</td>
+          <td>25</td>
+          <td>Male</td>
+          <td>Anom</td>
+          <td>19</td>
+          <td>Male</td>
+        </tr>
+      </table>
     </div>
   );
 }
