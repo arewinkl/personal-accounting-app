d6b45298c41ab6b39e86c582bae45a1b43ab125c
Alec Rewinkel <alecrewinkel@gmail.com>
2022-07-12 18:50:27 -0600

    links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import InputFields from "./components/input-fields";
+import Table from "./components/table";
 function App() {
   return (
     <div className="App">
       <InputFields />
+      <Table />
     </div>
   );
 }
--- src/components/table.js
+++ src/components/table.js
@@ -2,7 +2,11 @@ import { useState } from "react";
 import "../App.css";
 
 function Table() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>Ella dog</h2>
+    </div>
+  );
 }
 
 export default Table;
